ae556f65e43466a1e0d2e58fd163f04de17fb923
Ian <62034540+i13e@users.noreply.github.com>
2022-02-16 16:08:42 -0500

    Update autosnap-wrapper.sh

--- autosnap-wrapper.sh
+++ autosnap-wrapper.sh
@@ -5,10 +5,12 @@
 # during the same update while using a supported AUR helper
 
 # Automatically set which AUR helper to use
-if [ -x /usr/bin/paru ] ; then
-    export AUR_HELPER='/usr/bin/paru'
+if [ -n "$AUR_HELPER" ] ; then
+    echo "AUR_HELPER has been manually set to $AUR_HELPER by the user" # allow manually setting the AUR helper to be used
+elif [ -x /usr/bin/paru ] ; then
+    export AUR_HELPER='/usr/bin/paru' # if both are installed preference goes to paru
 elif [ -x /usr/bin/yay ] ; then
-    export AUR_HELPER='/usr/bin/yay'
+    export AUR_HELPER='/usr/bin/yay' 
 else
     echo "warning: please install a supported aur helper (paru or yay) to use this script." >&2
 fi
